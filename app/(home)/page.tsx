9a753652ba1178e1defb430e5b0c0ac981e88eed
leejaeyoung <leejy_222@naver.com>
2024-02-26 00:46:41 +0900

    server side & loading component (#3.3 finish)

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,23 @@
 export const metadata = {
-    title: "HOME",
+    title: "Home"
 }
 
-export default function Home() {
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+
+async function getMovies() {
+    
+    await new Promise((resolve) => setTimeout(resolve,3000));
+    const response = await fetch(URL);
+    const json = await response.json();
+    return json;
+}
+
+export default async function HomePage() {
+    const movies = await getMovies();
     return (
-        <h1>Home Page !</h1>
+        <div>
+            <h1>Home Page !</h1>
+            { JSON.stringify(movies) }
+        </div>
     )
 }
