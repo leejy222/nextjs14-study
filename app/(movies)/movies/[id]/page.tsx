78021d6a936385c036bc33e96386fe3063f3ed2e
이 재영 <leejy01022870502@gmail.com>
2024-02-27 17:22:34 +0900

    deployment

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,9 +1,20 @@
 import { Suspense } from "react";
 import { API_URL } from "../../../(home)/page" 
-import MovieInfo from "../../../../components/movie-info";
+import MovieInfo, { getMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
-export default async function MovieDetail({
+interface IParams {
+    params: { id: string };
+}
+
+export async function generateMetadata({params:{id}}: IParams) {
+    const movie = await getMovie(id);
+    return {
+        title: movie.title,
+    }
+}
+
+export default async function MovieDetailPage({
         params:{id}, 
     }: {
         params: {id:string}
